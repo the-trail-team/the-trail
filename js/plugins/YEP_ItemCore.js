08241018a6cf77fb23f665a3cddb9d1feaa3a847
NoLongerLucky <endroo04@gmail.com>
2021-03-17 18:19:26 -0400

    item limit no longer shows in menu

--- js/plugins/YEP_ItemCore.js
+++ js/plugins/YEP_ItemCore.js
@@ -1397,7 +1397,8 @@ Yanfly.Item.Window_ItemList_drawItemNumber =
     Window_ItemList.prototype.drawItemNumber;
 Window_ItemList.prototype.drawItemNumber = function(item, dx, dy, dw) {
     if (DataManager.isIndependent(item)) {
-      this.drawItemCarryNumber(item, dx, dy, dw);
+      // this.drawItemCarryNumber(item, dx, dy, dw);
+      this.drawItemCarryNumber();
       return;
     }
     Yanfly.Item.Window_ItemList_drawItemNumber.call(this, item, dx, dy, dw);
