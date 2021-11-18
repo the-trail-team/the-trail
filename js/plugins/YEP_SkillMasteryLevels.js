fd2b56a09135251c7ae0499ca841bff055ca34eb
NoLongerLucky <endroo04@gmail.com>
2021-11-17 20:54:24 -0500

    added failsafe for this._gaugeH in YEP_SkillMasteryLevels

--- js/plugins/YEP_SkillMasteryLevels.js
+++ js/plugins/YEP_SkillMasteryLevels.js
@@ -1129,7 +1129,11 @@ Window_Base.prototype.drawSkillMasteryGauge = function(skill, x, y, width) {
   width -= Window_Base._iconWidth + 4;
   var color1 = this.textColor(Yanfly.Param.SMLGauge1);
   var color2 = this.textColor(Yanfly.Param.SMLGauge2);
-  gaugeH = this._gaugeH;
+  if (this._gaugeH !== undefined) {
+    gaugeH = this._gaugeH;
+  } else {
+    var gaugeH = Yanfly.Param.SMLGaugeH;
+  }
   var gaugeY = y + this.lineHeight() - gaugeH - 2;
   if (Yanfly.Param.SMLGaugeOutline) {
     gaugeY -= 2;
