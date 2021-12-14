ae5c3c7db4b2e7c7a2b65cc83114311f41dcf35c
NoLongerLucky <endroo04@gmail.com>
2021-12-14 18:00:25 -0500

    added new tutorial tip hinting at attack accuracy formula

--- js/plugins/AP_TutorialTips.js
+++ js/plugins/AP_TutorialTips.js
@@ -51,7 +51,8 @@ const tips = [
 	"Level up your skills by using them! Each level requires 5 more uses than the previous... 275 in total for level 10.",
 	"Each level of skill mastery boosts damage/healing by 2% and reduces MP cost by 2%.",
 	"Don't forget about the skill learn menu! Not every skill you can get will be learned just by leveling up.",
-	"The shop menu's stat comparison for accessories will compare the accessory you are buying to whatever is equipped in the first accessory slot."
+	"The shop menu's stat comparison for accessories will compare the accessory you are buying to whatever is equipped in the first accessory slot.",
+	"Even a 100% hit rate doesn't guarantee every attack to land. Accuracy is just hit rate minus evasion, after all."
 ]
 
 tips[28] = "There's a total of " + tips.length + " Tutorial Tips that you can see here. Have you seen all of them?"; // tips.length is first defined with the array, so it cannot be used initially
