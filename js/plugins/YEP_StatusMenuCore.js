e5cde7690d7d4e522df8871983596c3020ad8612
NoLongerLucky <endroo04@gmail.com>
2022-11-08 10:54:47 -0500

    removed Job Points
    sometimes less is more...

--- js/plugins/YEP_StatusMenuCore.js
+++ js/plugins/YEP_StatusMenuCore.js
@@ -1319,10 +1319,6 @@ Window_StatusInfo.prototype.drawAttributeData = function(attr, dx, dy, dw) {
       this.drawAttributeName("Outgoing Healing Rate (OHR)", dx, dy, dw);
       this.drawAttributeRate(actor.ohr, dx, dy, dw);
       break;
-    case 'jpr':
-      this.drawAttributeName("JP Rate (JPR)", dx, dy, dw);
-      this.drawAttributeRate(actor.jpRate(), dx, dy, dw);
-      break;
     case 'pls':
       this.drawAttributeName("Physical Life Steal (PLS)", dx, dy, dw);
       this.drawAttributeRate(actor.getLifeStealRate('hpPhysicalRate'), dx, dy, dw);
