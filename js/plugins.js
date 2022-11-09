the-trail-team/the-trail

commit 041cc123dd78a9fc513f3b1e3c853be004322e95

refactored a few images

js/plugins.js
@@ -39,7 +39,7 @@ var $plugins =
 {"name":"YEP_BattleAICore","status":true,"description":"v1.15 This plugin allows you to structure battle A.I.\r\npatterns with more control.","parameters":{"Dynamic Actions":"true","Dynamic Turn Count":"true","Element Testing":"true","Default AI Level":"100"}},
 {"name":"YEP_X_ActorAutoBattleAI","status":true,"description":"v1.01 (Req YEP_BattleAICore) Give auto battle actors the same\r\ntype of A.I. that you can assign using the Battle AI Core's notetags.","parameters":{"Default AI Level":"100","Bypass Requirement":"[\"1\",\"2\"]","Curate Skill List":"true","Undecided AI":"false"}},
 {"name":"YEP_BattleBgmControl","status":true,"description":"v1.01 Assign BGM's to certain troops. Make BGM's change\r\nduring battle when certain enemies reach different HP values.","parameters":{}},
-{"name":"YEP_BattleSelectCursor","status":true,"description":"v1.04 Adds a cursor to battle when selecting allies and/or\r\nenemies for targeting.","parameters":{"Ally Cursor":"Crystal_Blue_4x2","Enemy Cursor":"Crystal_Red_4x2","Anchor X":"center","Anchor Y":"bottom","Position X":"center","Position Y":"top"}},
+{"name":"YEP_BattleSelectCursor","status":true,"description":"v1.04 Adds a cursor to battle when selecting allies and/or\r\nenemies for targeting.","parameters":{"Ally Cursor":"CrystalBlue","Enemy Cursor":"CrystalRed","Anchor X":"center","Anchor Y":"bottom","Position X":"center","Position Y":"top"}},
 {"name":"YEP_BuffsStatesCore","status":true,"description":"v1.16 Alter the basic mechanics behind buffs and states\r\nthat aren't adjustable within the RPG Maker editor.","parameters":{"---Turn Indicator---":"","Show Turns":"true","Font Size":"12","Turn Alignment":"right","Turn Buffer X":"-3","Turn Buffer Y":"-6","State Color":"0","Buff Color":"24","Debuff Color":"2","---Enemy Icons---":"","Show Enemy Icons":"true","Enemy Buff Turn":"true","Enemy State Turn":"true","Enemy State Counter":"true","---Buff Settings---":"","Default Limit":"5","Maximum Limit":"50","Buff Formula":"this._buffs[paramId] * 0.10 + 1.0","Show Buff Rate":"true","---State Settings---":"","Reapply Rules":"1","Show Enemy Turns":"true","---Counter Settings---":"","Counter Font Size":"16","Counter Alignment":"center","Counter Buffer X":"0","Counter Buffer Y":"8","Counter Color":"0"}},
 {"name":"YEP_X_ExtDoT","status":true,"description":"v1.04 (Req YEP_BattleEngineCore & YEP_BuffsStatesCore)\r\nCreate custom DoT formulas and effects with ease.","parameters":{"---Defaults---":"","Regen Animation":"0","DoT Animation":"0","Default Variance":"10","Default Element":"0"}},
 {"name":"YEP_X_StateCategories","status":true,"description":"v1.07 (Requires YEP_BuffsStatesCore.js) Sets up categories\r\nfor your states to make control over them easier.","parameters":{}},
@@ -182,7 +182,7 @@ var $plugins =
 {"name":"GALV_MoveRouteExtras","status":true,"description":"Additional SCRIPT commands to use within MOVE ROUTES\r\nView the plugin \"Help\" to view available commands.","parameters":{}},
 {"name":"GALV_MessageBusts","status":true,"description":"(v.2.8) Displays a bust image instead of selected face image","parameters":{"Bust Priority":"1","Bust Position":"0","Text X Offset":"390","Fade Out Speed":"40","Filename Append":""}},
 {"name":"GALV_Fishing","status":true,"description":"(v.1.7) Fishing mini game that runs in it's own scene. For advanced users.","parameters":{"--- OPTIONS ---":"","Lose Bait":"true","--- GRAPHICS ---":"","Surface Y":"281","Fisher Info":"1194,246","Fisher Rod XY Offset":"-8,-25","Default Rod Graphic":"rod","Default Bait Graphic":"bait","Equip Slot Graphic":"equipslot","Rod Equip XY":"40,40","Bait Equip XY":"100,40","No Rod Text":"Rod","No Bait Text":"Bait","No RodBait Text Size":"14","Cast Power Graphic":"castpower","Cast Power Frames":"11","Cast Pow XY":"410,200","Splash Graphic":"splash","-- SOUND/ANIM --":"","Default Music":"map_adon,80,100","Rod Equip SE":"Equip1,80,100","Bait Equip SE":"Equip1,80,100","Cast SE":"Wind4,80,120","Reel SE":"","Splash SE":"Water1,80,140","Hooked Anim":"1","Break Anim":"1","Player Catch Anim":"120","--- MENU ---":"","Menu Pos":"480,240,240","Cast Text":"Cast Line","Equip Rod Text":"Equip Rod","Equip Bait Text":"Equip Bait","Leave Text":"Stop Fishing","Catch Window":"430,270,420,180","Caught Text":"You caught a","Equip Window Width":"72","Equip Window Cols":"1","Length Text":"Length","Weight Text":"Weight","-- RECORDS --":"","Record Title":"FISH STATS","Record Types":"Fish Types","Record Total":"Total Caught","Record Fish":"Record Fish","Record Fish Number":"Number Caught","Record Fish Length":"Record Length","Record Fish Weight":"Record Weight","Record Scene Padding":"40","Record Fish Y":"150","Record Fish Scale":"1.5","--- FISH ---":"","Fish 1":"fish_carp,8,0,1,2|8,-1,126,1,45|4,true,40|80,0.2|1.4,,0,","Fish 2":"fish_dace,10,1,1,2|8,-1,127,1,45|4,true,10|15,0.05|0.15,,0,","Fish 3":"fish_catfish,10,1,1,2|8,-1,220,1,45|4,true,30|60,0.1|0.2,,0,","Fish 4":"fish_crab,1,7,1,10|10,-1,222,2,30|3,true,10|25,0.125|0.5,,0,","Fish 5":"","Fish 6":"","Fish 7":"","Fish 8":"","Fish 9":"","Fish 10":"","Fish 11":"","Fish 12":"","Fish 13":"","Fish 14":"","Fish 15":"","Fish 16":"","Fish 17":"","Fish 18":"","Fish 19":"","Fish 20":"","Fish 21":"","Fish 22":"","Fish 23":"","Fish 24":"","Fish 25":"","Fish 26":"","Fish 27":"","Fish 28":"","Fish 29":"","Fish 30":"","Fish 31":"","Fish 32":"","Fish 33":"","Fish 34":"","Fish 35":"","Fish 36":"","Fish 37":"","Fish 38":"","Fish 39":"","Fish 40":"","Fish 41":"","Fish 42":"","Fish 43":"","Fish 44":"","Fish 45":"","Fish 46":"","Fish 47":"","Fish 48":"","Fish 49":"","Fish 50":""}},
-{"name":"Galv_NewItemIndication","status":true,"description":"(v.1.2) Adds a 'new' icon over items that have been obtained for the first time","parameters":{"New Icon Image":"newIcon","Icon X Offset":"0","Icon Y Offset":"0"}},
+{"name":"Galv_NewItemIndication","status":true,"description":"(v.1.2) Adds a 'new' icon over items that have been obtained for the first time","parameters":{"New Icon Image":"NewIcon","Icon X Offset":"0","Icon Y Offset":"0"}},
 {"name":"GALV_StationaryTurn","status":true,"description":"(v.1.3) Change movement so the player turns on the spot if direction is tapped.","parameters":{"Delay":"5"}},
 {"name":"GALV_BasicEventShadows","status":true,"description":"(v.1.2) Display shadows under the player or events on the map.","parameters":{"Shadow Image":"Shadow1","Shadow Offset":"5"}},
 {"name":"GALV_MenuFade","status":true,"description":"Adds a fade in and fade out to the main menu only.","parameters":{"Fade Speed":"10","Dont Fade Scenes":"Scene_Title,Scene_Load,Scene_Name"}},

commit 23e573dc724835cffa8481e1556f123061a60e68

fixed battle select cursor names
broken by 041cc123dd78a9fc513f3b1e3c853be004322e95

js/plugins.js
@@ -39,7 +39,7 @@ var $plugins =
 {"name":"YEP_BattleAICore","status":true,"description":"v1.15 This plugin allows you to structure battle A.I.\r\npatterns with more control.","parameters":{"Dynamic Actions":"true","Dynamic Turn Count":"true","Element Testing":"true","Default AI Level":"100"}},
 {"name":"YEP_X_ActorAutoBattleAI","status":true,"description":"v1.01 (Req YEP_BattleAICore) Give auto battle actors the same\r\ntype of A.I. that you can assign using the Battle AI Core's notetags.","parameters":{"Default AI Level":"100","Bypass Requirement":"[\"1\",\"2\"]","Curate Skill List":"true","Undecided AI":"false"}},
 {"name":"YEP_BattleBgmControl","status":true,"description":"v1.01 Assign BGM's to certain troops. Make BGM's change\r\nduring battle when certain enemies reach different HP values.","parameters":{}},
-{"name":"YEP_BattleSelectCursor","status":true,"description":"v1.04 Adds a cursor to battle when selecting allies and/or\r\nenemies for targeting.","parameters":{"Ally Cursor":"CrystalBlue","Enemy Cursor":"CrystalRed","Anchor X":"center","Anchor Y":"bottom","Position X":"center","Position Y":"top"}},
+{"name":"YEP_BattleSelectCursor","status":true,"description":"v1.04 Adds a cursor to battle when selecting allies and/or\r\nenemies for targeting.","parameters":{"Ally Cursor":"CrystalBlue_4x2","Enemy Cursor":"CrystalRed_4x2","Anchor X":"center","Anchor Y":"bottom","Position X":"center","Position Y":"top"}},
 {"name":"YEP_BuffsStatesCore","status":true,"description":"v1.16 Alter the basic mechanics behind buffs and states\r\nthat aren't adjustable within the RPG Maker editor.","parameters":{"---Turn Indicator---":"","Show Turns":"true","Font Size":"12","Turn Alignment":"right","Turn Buffer X":"-3","Turn Buffer Y":"-6","State Color":"0","Buff Color":"24","Debuff Color":"2","---Enemy Icons---":"","Show Enemy Icons":"true","Enemy Buff Turn":"true","Enemy State Turn":"true","Enemy State Counter":"true","---Buff Settings---":"","Default Limit":"5","Maximum Limit":"50","Buff Formula":"this._buffs[paramId] * 0.10 + 1.0","Show Buff Rate":"true","---State Settings---":"","Reapply Rules":"1","Show Enemy Turns":"true","---Counter Settings---":"","Counter Font Size":"16","Counter Alignment":"center","Counter Buffer X":"0","Counter Buffer Y":"8","Counter Color":"0"}},
 {"name":"YEP_X_ExtDoT","status":true,"description":"v1.04 (Req YEP_BattleEngineCore & YEP_BuffsStatesCore)\r\nCreate custom DoT formulas and effects with ease.","parameters":{"---Defaults---":"","Regen Animation":"0","DoT Animation":"0","Default Variance":"10","Default Element":"0"}},
 {"name":"YEP_X_StateCategories","status":true,"description":"v1.07 (Requires YEP_BuffsStatesCore.js) Sets up categories\r\nfor your states to make control over them easier.","parameters":{}},