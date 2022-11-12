the-trail-team/the-trail

commit dacd5f8496a0f23d49615850a7c3bcd5e900e2c9

weapons and armor separated into all equip types in category menu

js/plugins.js
@@ -195,7 +195,7 @@ var $plugins =
 {"name":"AP_Proficiencies","status":true,"description":"","parameters":{}},
 {"name":"--------------------","status":false,"description":"------------------------------------------------------------","parameters":{}},
 {"name":"--------------------","status":false,"description":"------------------------------------------------------------","parameters":{}},
-{"name":"ARP_CommandIcons","status":true,"description":"v1.01 Adds Icons to command windows\r\n<ARP_ComIcons>","parameters":{"Scenes To Draw":"[\"Scene_Menu\",\"Scene_Title\",\"Scene_Equip\",\"Scene_Status\",\"Scene_Item\",\"Scene_Battle\",\"Scene_Shop\",\"Scene_Skill\",\"Scene_GameEnd\",\"Scene_Save\",\"Scene_Load\",\"Scene_Synthesis\",\"Scene_EquipCustomize\",\"Scene_Gameover\",\"Scene_Fishing\",\"Scene_Proficiency\"]","Command Icons":"[\"Items: 560\",\"Archaic Hourglass: 280\",\"Skills: 115\",\"Learn Skills: 14\",\"Special: 164\",\"Magic: 165\",\"Passive: 121\",\"Equipment: 121\",\"Equipment: 162\",\"Status: 234\",\"Team Setup: 121\",\"Quest Log: 230\",\"Music: 1366\",\"Options: 1363\",\"Save & Load: 1360\",\"Debug: 341\",\"Close Game: 368\",\"To Title: 12\",\"New Game: 224\",\"Load Game: 1361\",\"Credits: 495\",\"Tutorial Tips: 1367\",\"Shut Down: 6\",\"Equip: 170\",\"Customize: 232\",\"Optimize: 495\",\"Clear: 119\",\"General: 121\",\"Stats: 14\",\"Attributes: 12\",\"Info: 234\",\"Elements: 96\",\"States: 104\",\"Cast Line: 116\",\"Equip Rod: 1250\",\"Equip Bait: 1248\",\"Stop Fishing: 12\",\"Items: 261\",\"Key Items: 243\",\"Weapons: 147\",\"Armor: 170\",\"Recovery: 192\",\"Buffs: 269\",\"Debuffs: 319\",\"Materials: 331\",\"Drops: 196\",\"Foodstuffs: 297\",\"Fishing: 1250\",\"Upgraders: 232\",\"Salvaging: 675\",\"Memorabilia: 518\",\"Vanity: 442\",\"Usable: 307\",\"Field: 224\",\"Battle: 551\",\"Misc.: 201\",\"Boost: 187\",\"Unboost: 184\",\"Guard: 13\",\"Fight: 116\",\"Run Away: 12\",\"Buy: 262\",\"Sell: 512\",\"Skills: 115\",\"Sure you quittin'?: 1\",\"Cancel: 119\",\"Load: 1361\",\"Save: 1360\",\"Delete: 119\",\"Rename Save: 1362\",\"Craft Item: 331\",\"Craft Weapon: 391\",\"Craft Armor: 493\",\"Craft Headgear: 659\",\"Craft Bodygear: 169\",\"Craft Footgear: 474\",\"Craft Offhand: 507\",\"Craft Accessory: 511\",\"Craft Magic Equip: 368\",\"Try Again: 338\",\"Proficiencies: 14\",\"Mining: 276\",\"Farming: 1266\",\"Gathering: 302\",\"Cooking: 1298\",\"Fishing: 1250\"]","=== WEAPON ICONS ===":"","Draw Weapon Icon":"true","Barehanded Icon":"116","Weapon Unique Icons":"0"}},
+{"name":"ARP_CommandIcons","status":true,"description":"v1.01 Adds Icons to command windows\r\n<ARP_ComIcons>","parameters":{"Scenes To Draw":"[\"Scene_Menu\",\"Scene_Title\",\"Scene_Equip\",\"Scene_Status\",\"Scene_Item\",\"Scene_Battle\",\"Scene_Shop\",\"Scene_Skill\",\"Scene_GameEnd\",\"Scene_Save\",\"Scene_Load\",\"Scene_Synthesis\",\"Scene_EquipCustomize\",\"Scene_Gameover\",\"Scene_Fishing\",\"Scene_Proficiency\"]","Command Icons":"[\"Items: 560\",\"Archaic Hourglass: 280\",\"Skills: 115\",\"Learn Skills: 14\",\"Special: 164\",\"Magic: 165\",\"Passive: 121\",\"Equipment: 121\",\"Equipment: 162\",\"Status: 234\",\"Team Setup: 121\",\"Quest Log: 230\",\"Music: 1366\",\"Options: 1363\",\"Save & Load: 1360\",\"Debug: 341\",\"Close Game: 368\",\"To Title: 12\",\"New Game: 224\",\"Load Game: 1361\",\"Credits: 495\",\"Tutorial Tips: 1367\",\"Shut Down: 6\",\"Equip: 170\",\"Customize: 232\",\"Optimize: 495\",\"Clear: 119\",\"General: 121\",\"Stats: 14\",\"Attributes: 12\",\"Info: 234\",\"Elements: 96\",\"States: 104\",\"Cast Line: 116\",\"Equip Rod: 1250\",\"Equip Bait: 1248\",\"Stop Fishing: 12\",\"Items: 261\",\"Key Items: 243\",\"Weapons: 147\",\"Armor: 170\",\"Recovery: 192\",\"Buffs: 269\",\"Debuffs: 319\",\"Materials: 331\",\"Drops: 196\",\"Foodstuffs: 297\",\"Fishing: 1250\",\"Upgraders: 232\",\"Salvaging: 675\",\"Memorabilia: 518\",\"Vanity: 442\",\"Usable: 307\",\"Field: 224\",\"Battle: 551\",\"Misc.: 201\",\"Boost: 187\",\"Unboost: 184\",\"Guard: 13\",\"Fight: 116\",\"Run Away: 12\",\"Buy: 262\",\"Sell: 512\",\"Skills: 115\",\"Sure you quittin'?: 1\",\"Cancel: 119\",\"Load: 1361\",\"Save: 1360\",\"Delete: 119\",\"Rename Save: 1362\",\"Craft Item: 331\",\"Craft Weapon: 391\",\"Craft Armor: 493\",\"Craft Headgear: 659\",\"Craft Bodygear: 169\",\"Craft Footgear: 474\",\"Craft Offhand: 507\",\"Craft Accessory: 511\",\"Craft Magic Equip: 368\",\"Try Again: 338\",\"Proficiencies: 14\",\"Mining: 276\",\"Farming: 1266\",\"Gathering: 302\",\"Cooking: 1298\",\"Fishing: 1250\",\"Generic: 433\",\"Sword: 389\",\"Greatsword: 398\",\"Axe: 403\",\"Spear: 146\",\"Wand: 417\",\"Spellbook: 1234\",\"Staff: 411\",\"Hammer: 412\",\"Dagger: 150\",\"Knuckles: 408\",\"Heavy Armor: 438\",\"Robes: 440\",\"Vestments: 623\",\"Leathers: 455\",\"Light Shield: 160\",\"Heavy Shield: 161\",\"Headgear: 457\",\"Bodygear: 434\",\"Footgear: 467\",\"Offhand: 502\",\"Accessory: 177\",\"Magic Equip: 182\",\"Power: 125\"]","=== WEAPON ICONS ===":"","Draw Weapon Icon":"true","Barehanded Icon":"116","Weapon Unique Icons":"0"}},
 {"name":"ARP_TitleCommandExit","status":true,"description":"v1.00 Adds an option in the title command window to close\r\ngame window.","parameters":{"Command Exit":"Shut Down","Show Exit":"true"}},
 {"name":"Irina_AutoMessageColor","status":true,"description":"<AutoMessageColor> RPG Maker MV version 1.6.2","parameters":{"TextCodeHighlighting":"","TextCodeBase":"..........","TCActorName":"1","TCPartyName":"1","TextCodeYepMsgCore":"..........","TCActorClass":"4","TCActorNickname":"4","TCPartyClass":"4","TCPartyNickname":"4","TCClassName":"5","TCItemName":"3","TCWeaponName":"3","TCArmorName":"3","TCEnemyName":"3","TCSkillName":"3","TCStateName":"3","TCItemIcon":"3","TCWeaponIcon":"3","TCArmorIcon":"3","TCSkillIcon":"3","TCStateIcon":"3","TextCodeYepExtMsg1":"..........","TCEnemyNamePack1":"4","TCEnemyTroop":"4","":"","WordHighlighting":"","TextColor1":"[]","TextColor2":"[]","TextColor3":"[]","TextColor4":"[]","TextColor5":"[]","TextColor6":"[]","TextColor7":"[]","TextColor8":"[]","TextColor9":"[]","TextColor10":"[]","TextColor11":"[]","TextColor12":"[]","TextColor13":"[]","TextColor14":"[]","TextColor15":"[]","TextColor16":"[]","TextColor17":"[]","TextColor18":"[\"Apocalypse\"]","TextColor19":"[]","TextColor20":"[]","TextColor21":"[]","TextColor22":"[]","TextColor23":"[]","TextColor24":"[]","TextColor25":"[]","TextColor26":"[]","TextColor27":"[]","TextColor28":"[]","TextColor29":"[]","TextColor30":"[]","TextColor31":"[]"}},
 {"name":"CP_Large_Sprite_Fix","status":true,"description":"Fixes an issue with large sprites appearing under ☆ passability tiles.","parameters":{"Terrain ID":"7","Large Followers":"Yes","Large Vehicles":"Yes"}},

js/plugins/YEP_X_ItemCategories.js
@@ -271,9 +271,19 @@ Window_ItemCategory.prototype.update = function() {
 Window_ItemCategory.prototype.makeCommandList = function() {
   var data = Yanfly.Param.ItemCatOrder;
   var length = data.length;
-  for (var i = 0; i < length; i++) {
+  if (this._type == '') for (var i = 0; i < length; i++) {
     var category = data[i].trim();
     this.addItemCategory(category);
+  } else if (this._type == 'weapons') {
+    this.addItemCategory("Weapons");
+    var wtypes = [1, 2, 11, 7, 6, 3, 10, 4, 8, 5, 9];
+    for (i = 0; i < wtypes.length; i++) this.addItemCategory("WType:" + wtypes[i]);
+  } else if (this._type == 'armors') {
+    this.addItemCategory("Armors");
+    var atypes = [1, 2, 3, 4, 5, 6, 7];
+    for (i = 0; i < atypes.length; i++) this.addItemCategory("AType:" + atypes[i]);
+    var etypes = [3, 4, 5, 2, 6, 8, 9];
+    for (i = 0; i < etypes.length; i++) this.addItemCategory("EType:" + etypes[i]);
   }
 };
 

js/rpg_scenes.js
@@ -1245,6 +1245,8 @@ Scene_Item.prototype.createCategoryWindow = function() {
     this._categoryWindow.y = this._helpWindow.height;
     this._categoryWindow.setHandler('ok',     this.onCategoryOk.bind(this));
     this._categoryWindow.setHandler('cancel', this.popScene.bind(this));
+    this._categoryWindow.setHandler('weapon', this.commandWeapon.bind(this));
+    this._categoryWindow.setHandler('armor',  this.commandArmor.bind(this));
     this.addWindow(this._categoryWindow);
 };
 
@@ -1295,6 +1297,22 @@ Scene_Item.prototype.useItem = function() {
     Scene_ItemBase.prototype.useItem.call(this);
     this._itemWindow.redrawCurrentItem();
 };
+
+Scene_Item.prototype.commandWeapon = function() {
+    if (this._categoryWindow._type != '') return;
+    this._categoryWindow._type = 'weapons';
+    this._categoryWindow.select(0);
+    this._categoryWindow.activate();
+    this._categoryWindow.refresh();
+};
+  
+Scene_Item.prototype.commandArmor = function() {
+    if (this._categoryWindow._type != '') return;
+    this._categoryWindow._type = 'armors';
+    this._categoryWindow.select(0);
+    this._categoryWindow.activate();
+    this._categoryWindow.refresh();
+};  
 
 //-----------------------------------------------------------------------------
 // Scene_Skill

js/plugins/YEP_ItemCore.js
@@ -2197,7 +2197,7 @@ Scene_Item.prototype.createItemWindow = function() {
     this.createStatusWindow();
     this.createInfoWindow();
     this.createActionWindow();
-    this._categoryWindow.setHandler('cancel', this.exitScene.bind(this));
+    this._categoryWindow.setHandler('cancel', this.onCategoryCancel.bind(this));
 };
 
 Scene_Item.prototype.createStatusWindow = function() {
@@ -2270,6 +2270,16 @@ Scene_Item.prototype.exitScene = function() {
       if (member) member.refresh();
     }
     this.popScene();
+};
+
+Scene_Item.prototype.onCategoryCancel = function() {
+    if (this._categoryWindow._type == '') this.popScene();
+    else {
+      this._categoryWindow.select(this._categoryWindow._type == 'weapons' ? 2 : 3);
+      this._categoryWindow._type = '';
+      this._categoryWindow.activate();
+      this._categoryWindow.refresh();
+    }
 };
 
 //=============================================================================

js/rpg_windows.js
@@ -1868,6 +1868,7 @@ Window_ItemCategory.prototype.constructor = Window_ItemCategory;
 
 Window_ItemCategory.prototype.initialize = function() {
     Window_HorzCommand.prototype.initialize.call(this, 0, 0);
+    this._type = '';
 };
 
 Window_ItemCategory.prototype.windowWidth = function() {

commit ed6fa06a052c813487c0cc26afb26eb82b445fed

fixed freeze when trying to view all weapons/armors
caused by dacd5f8496a0f23d49615850a7c3bcd5e900e2c9

js/plugins.js
@@ -62,7 +62,7 @@ var $plugins =
 {"name":"--------------------","status":false,"description":"------------------------------------------------------------","parameters":{}},
 {"name":"YEP_ItemCore","status":true,"description":"v1.30 Changes the way Items are handled for your game\r\nand the Item Scene, too.","parameters":{"---General---":"","Max Items":"0","Max Weapons":"Number.MAX_SAFE_INTEGER","Max Armors":"Number.MAX_SAFE_INTEGER","Starting ID":"3000","Random Variance":"0","Negative Variance":"false","Name Format":"\\c[8]%1\\c[0]%2\\c[8]%3%4\\c[0]","Name Spacing":"true","Boost Format":"+%1","---Item Scene---":"","Updated Scene Item":"true","List Equipped Items":"true","Show Icon":"true","Icon Size":"128","Font Size":"20","Command Alignment":"center","Recovery Format":"%1 Heal","Add State":"+State","Add Buff":"+Buff","Remove State":"-State","Remove Buff":"-Buff","Maximum Icons":"99","Use Command":"Use %1","Carry Format":"","--Independent Items--":"","Midgame Note Parsing":"false"}},
 {"name":"YEP_X_ItemDisassemble","status":true,"description":"v1.06 (Requires YEP_ItemCore.js) Grants the option to\nbreak down items in the item menu into other items.","parameters":{"---General---":"","Disassemble Command":"Salvage %1","Disassemble List":"Salvage Items","Item Quantity 1":"×%1 %2","Item Quantity 2":"×%1-×%2 %3","Rate Font Size":"20","---Disassemble Sound---":"","Disassemble Sound":"Break","Disassemble Volume":"100","Disassemble Pitch":"150","Disassemble Pan":"0","---Result Sound---":"","Result Sound":"Equip3","Result Volume":"100","Result Pitch":"100","Result Pan":"0","---Empty Sound---":"","Empty Sound":"Buzzer1","Empty Volume":"100","Empty Pitch":"100","Empty Pan":"0"}},
-{"name":"YEP_X_ItemCategories","status":true,"description":"v1.01 (Requires YEP_ItemCore.js) Assign items to various\r\ncategories under the item menu.","parameters":{"---General---":"","Category Order":"Items,KeyItems,Weapons,Armors,Category:Recovery,Category:Buffs,Category:Debuffs,Category:Materials,Category:Drops,Category:Mining,Category:Foodstuffs,Category:Fishing,Category:Upgraders,Category:Salvaging,Category:Memorabilia,Category:Vanity","---Naming---":"","Hidden Item A":"","Hidden Item B":"","Consumable":"Consumable","Nonconsumable":"Nonconsumable","Always Usable":"Usable","Battle Usable":"Battle","Field Usable":"Field","Never Usable":"Misc."}},
+{"name":"YEP_X_ItemCategories","status":true,"description":"v1.01 (Requires YEP_ItemCore.js) Assign items to various\r\ncategories under the item menu.","parameters":{"---General---":"","Category Order":"Items,KeyItems,WeaponCat,ArmorCat,Category:Recovery,Category:Buffs,Category:Debuffs,Category:Materials,Category:Drops,Category:Mining,Category:Foodstuffs,Category:Fishing,Category:Upgraders,Category:Salvaging,Category:Memorabilia,Category:Vanity","---Naming---":"","Hidden Item A":"","Hidden Item B":"","Consumable":"Consumable","Nonconsumable":"Nonconsumable","Always Usable":"Usable","Battle Usable":"Battle","Field Usable":"Field","Never Usable":"Misc."}},
 {"name":"YEP_X_ItemRename","status":true,"description":"v1.01 (Requires YEP_ItemCore.js) Allows the player to\r\nrename independent items in their inventory.","parameters":{"Rename Command":"Rename %1","Default Rename":"true","Max Length":"32","Rename Text":"...will be renamed to..."}},
 {"name":"YEP_X_ItemRequirements","status":true,"description":"v1.02 (Requires YEP_ItemCore.js) Place requirements on\r\nitems before they can be used.","parameters":{}},
 {"name":"YEP_X_ItemUpgradeSlots","status":true,"description":"v1.10 (Requires YEP_ItemCore.js) Allows independent items to\r\nbe upgradeable and gain better stats.","parameters":{"Default Slots":"0","Slot Variance":"0","Upgrade Command":"Upgrade %1","Show Only":"true","Slots Available":"Upgrade Slots Available:","Show Slot Upgrades":"true","Slot Upgrade Format":"\\}Slot %1: %2\\{","Default Sound":"Powerup"}},

js/plugins/YEP_X_ItemCategories.js
@@ -347,6 +347,12 @@ Window_ItemCategory.prototype.addItemCategory = function(category) {
     var id = parseInt(RegExp.$1);
     return this.addCommand($dataSystem.equipTypes[id], 'EType', true, id);
   }
+  if (category.match(/WeaponCat/i)) {
+    return this.addCommand(TextManager.weapon, 'WeaponCat');
+  }
+  if (category.match(/ArmorCat/i)) {
+    return this.addCommand(TextManager.armor, 'ArmorCat');
+  }
 };
 
 //=============================================================================

js/rpg_scenes.js
@@ -1243,10 +1243,10 @@ Scene_Item.prototype.createCategoryWindow = function() {
     this._categoryWindow = new Window_ItemCategory();
     this._categoryWindow.setHelpWindow(this._helpWindow);
     this._categoryWindow.y = this._helpWindow.height;
-    this._categoryWindow.setHandler('ok',     this.onCategoryOk.bind(this));
-    this._categoryWindow.setHandler('cancel', this.popScene.bind(this));
-    this._categoryWindow.setHandler('weapon', this.commandWeapon.bind(this));
-    this._categoryWindow.setHandler('armor',  this.commandArmor.bind(this));
+    this._categoryWindow.setHandler('ok',        this.onCategoryOk.bind(this));
+    this._categoryWindow.setHandler('cancel',    this.popScene.bind(this));
+    this._categoryWindow.setHandler('WeaponCat', this.commandWeapon.bind(this));
+    this._categoryWindow.setHandler('ArmorCat',  this.commandArmor.bind(this));
     this.addWindow(this._categoryWindow);
 };
 