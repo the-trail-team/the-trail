2c8a702dface811ff21de434cd60ba54b8e5ef2a
NoLongerLucky <endroo04@gmail.com>
2022-11-17 21:03:10 -0500

    events walk through followers, removed unneeded follower hiding

--- js/rpg_objects.js
+++ js/rpg_objects.js
@@ -7827,7 +7827,7 @@ Game_Player.prototype.isCollided = function(x, y) {
     if (this.isThrough()) {
         return false;
     } else {
-        return this.pos(x, y) || this._followers.isSomeoneCollided(x, y);
+        return this.pos(x, y);
     }
 };
 
