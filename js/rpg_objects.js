the-trail-team/the-trail

commit 50cad34d56b900d74be81820da065ae8905706ff

fixed states with death removal bypassing having steps reset upon death

js/rpg_objects.js
@@ -3695,7 +3695,6 @@ Game_Actor.prototype.battlerName = function() {
 
 Game_Actor.prototype.clearStates = function() {
     Game_Battler.prototype.clearStates.call(this);
-    this._stateSteps = {};
 };
 
 Game_Actor.prototype.eraseState = function(stateId) {

commit cbe262c4feb85b768706ca6ec50a8499e849025b

fixed crash when starting new game
caused by 50cad34d56b900d74be81820da065ae8905706ff which crashed due to no longer initializing stateSteps object. better fix has been implemented which should make clearStates() clear states again but still fixes the step bug.

js/rpg_objects.js
@@ -3711,6 +3711,8 @@ Game_Actor.prototype.battlerName = function() {
 
 Game_Actor.prototype.clearStates = function() {
     Game_Battler.prototype.clearStates.call(this);
+    if (!this._stateSteps) this._stateSteps = {};
+    for (const s in this._stateSteps) if (!$dataStates[s].category.contains("BYPASS DEATH REMOVAL")) delete this._stateSteps[s];
 };
 
 Game_Actor.prototype.eraseState = function(stateId) {