fd22103d2746f7f3596ad3a273f4b4a40d6c4002
NoLongerLucky <endroo04@gmail.com>
2023-01-16 01:01:09 -0500

    added $gameMap.indoors() function

--- js/rpg_objects.js
+++ js/rpg_objects.js
@@ -6519,6 +6519,10 @@ Game_Map.prototype.inTrueTelluriaCastle = function() {
     return [2, 88, 92, 93, 94, 213, 214].contains(this.mapId());
 };
 
+Game_Map.prototype.indoors = function() {
+    return [3, 4, 6, 10].contains(this.tilesetId());
+};
+
 //-----------------------------------------------------------------------------
 // Game_CommonEvent
 //
