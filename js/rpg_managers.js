70dc6218ec92035dbd58dc626e39208ff2b46536
NoLongerLucky <endroo04@gmail.com>
2023-02-01 00:25:54 -0500

    no longer attempts to load old Loading.png

--- js/rpg_managers.js
+++ js/rpg_managers.js
@@ -1855,7 +1855,7 @@ SceneManager.initGraphics = function() {
     Graphics.initialize(this._screenWidth, this._screenHeight, type);
     Graphics.boxWidth = this._boxWidth;
     Graphics.boxHeight = this._boxHeight;
-    Graphics.setLoadingImage('img/system/Loading.png');
+    // Graphics.setLoadingImage('img/system/Loading.png');
     if (Utils.isOptionValid('showfps')) {
         Graphics.showFps();
     }
