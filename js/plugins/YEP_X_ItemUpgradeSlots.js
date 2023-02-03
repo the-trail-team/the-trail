the-trail-team/the-trail

commit 595edb5f81693529df0ade1a305dccc87312116a

improved cleansing scroll independent item processing

js/plugins/YEP_X_ItemUpgradeSlots.js
@@ -809,13 +809,9 @@ ItemManager.effectIUSResetStat = function(item, stat) {
         var id = item.id;
         var item = JsonEx.makeDeepCopy(baseItem);
         item.id = id;
-        if (DataManager.isItem(baseItem)) $dataItems[id] = item;
-        if (DataManager.isWeapon(baseItem)) $dataWeapons[id] = item;
-        if (DataManager.isArmor(baseItem)) $dataArmors[id] = item;
+        DataManager.getDatabase(item)[id] = item;
         ItemManager.setNewIndependentItem(baseItem, item);
-        if (DataManager.isItem(item)) DataManager._independentItems[id - Yanfly.Param.ItemStartingId - 1] = item;
-        if (DataManager.isWeapon(item)) DataManager._independentWeapons[id - Yanfly.Param.ItemStartingId - 1] = item;
-        if (DataManager.isArmor(item)) DataManager._independentArmors[id - Yanfly.Param.ItemStartingId - 1] = item;
+        DataManager.getContainer(item)[id - Yanfly.Param.ItemStartingId - 1] = item;
         this._fullReset = true;
         this._resetItem = item;
         break;

commit a31e5702b8f7a8c7be5bdbeb56ce4ec628f1a6f0

fixed minor typo that broke Cleansing Scroll
broken by 595edb5f81693529df0ade1a305dccc87312116a

js/plugins/YEP_X_ItemUpgradeSlots.js
@@ -809,7 +809,7 @@ ItemManager.effectIUSResetStat = function(item, stat) {
         var id = item.id;
         var item = JsonEx.makeDeepCopy(baseItem);
         item.id = id;
-        DataManager.getDatabase(item)[id] = item;
+        DataManager.getDatabase(baseItem)[id] = item;
         ItemManager.setNewIndependentItem(baseItem, item);
         DataManager.getContainer(item)[id - Yanfly.Param.ItemStartingId - 1] = item;
         this._fullReset = true;