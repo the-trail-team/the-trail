68f0082c6947adfe622444b203c1ac1a8788c7d4
NoLongerLucky <endroo04@gmail.com>
2023-02-10 15:16:29 -0500

    fix crash when p2 talks without a magic equip
    caused by check for Infernal Coin not making sure that something is actually equipped

--- js/plugins/GALV_MessageBusts.js
+++ js/plugins/GALV_MessageBusts.js
@@ -354,8 +354,9 @@ Sprite_GalvBust.prototype.loadBitmap = function() {
 	if ($gameMessage.faceName() === "Actor4") {
 		if (index % 4 === 2 && $gameActors.actor(4).equips()[10]) { // Player 2
 			id = $gameActors.actor(2).equips()[10].baseItemId;
-			if ($gameActors.actor(2).equips()[8].baseItemId == 199) name += "_F";
-			else if (id === 121) name += "_C";
+			if ($gameActors.actor(2).equips()[8]) {
+				if ($gameActors.actor(2).equips()[8].baseItemId == 199) name += "_F";
+			} else if (id === 121) name += "_C";
 		}
 		if (index % 4 === 1 && $gameActors.actor(4).equips()[10]) { // Player 3
 			id = $gameActors.actor(3).equips()[10].baseItemId;
