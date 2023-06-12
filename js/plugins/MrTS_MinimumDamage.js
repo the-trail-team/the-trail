1e6531d2729c6e7eec40e06c283fd047d10d03e8
NoLongerLucky <endroo04@gmail.com>
2023-06-12 15:39:13 -0400

    Revert "removed minimum damage cap of 1"
    This reverts commit 08d2f273f3c09c1684b7d5f2ec2c8988cd233a14.

---
+++ js/plugins/MrTS_MinimumDamage.js
@@ -0,0 +1,36 @@
+//==============================================================================
+// MrTS_MinimumDamage.js
+//==============================================================================
+
+/*:
+* @plugindesc Changes minimum damage from 0.
+* @author Mr. Trivel
+* 
+* @param Minimum Damage
+* @desc Minimum Damage possible.
+* @default 1
+* 
+* @help 
+* --------------------------------------------------------------------------------
+* Terms of Use
+* --------------------------------------------------------------------------------
+* Don't remove the header or claim that you wrote this plugin.
+* Credit Mr. Trivel if using this plugin in your project.
+* Free for commercial and non-commercial projects.
+* --------------------------------------------------------------------------------
+* Version 1.1
+* --------------------------------------------------------------------------------
+*/
+
+(function() {
+	var parameters = PluginManager.parameters('MrTS_DamagePostfix');
+	var minDamage = Number(parameters['Minimum Damage'] || 1);
+
+	var _Game_Action_makeDamageValue = Game_Action.prototype.makeDamageValue;
+
+	Game_Action.prototype.makeDamageValue = function(target, critical) {
+		var value = _Game_Action_makeDamageValue.call(this, target, critical);
+		if (this._item) if (this._item._dataClass === "item") if (this._item._itemId === 106) return value; // Red Mushroom exception
+		return (value >= 0 ? Math.max(value, minDamage) : value);
+	};
+})();
