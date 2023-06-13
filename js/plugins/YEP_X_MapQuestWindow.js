06cfcdf885b70f611ac39f8b772d126c0f1ec8e7
NoLongerLucky <endroo04@gmail.com>
2023-06-12 21:43:00 -0400

    fixed Defeat Apocalypse automatically set active after loading game

--- js/plugins/YEP_X_MapQuestWindow.js
+++ js/plugins/YEP_X_MapQuestWindow.js
@@ -551,7 +551,7 @@ if (Yanfly.Param.MQWAddQuest) {
 Yanfly.AMQW.Game_System_questAdd = Game_System.prototype.questAdd;
 Game_System.prototype.questAdd = function(questId) {
   Yanfly.AMQW.Game_System_questAdd.call(this, questId);
-  if (this._questsKnown.contains(questId)) return;
+  if (questId == 4 && $gameMap.mapId() != 17) return;
   this.setActiveQuestId(questId);
 };
 
