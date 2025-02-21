339531194b04b36fe9adb887a82619a276d5c81c
apotb <endroo04@gmail.com>
2025-02-20 20:36:27 -0500

    fixed game going out of fullscreen if fullscreened while loading

--- js/plugins/Fullscreen.js
+++ js/plugins/Fullscreen.js
@@ -19,7 +19,7 @@
   }
  
   extend(Scene_Boot, 'start', function() {
-		Graphics._switchFullScreen();
+		if (Graphics._isFullScreen()) Graphics._switchFullScreen();
   })
   
   extend(Scene_Base, 'create', function() {
